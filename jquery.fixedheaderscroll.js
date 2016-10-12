6a409d4f2dce3a57cdd90951ac72ae75c2174893
TakumiNakazawa <nakazawa@tasko.jp>
2016-10-12 17:19:23 +0900

    複数回コールしても、1回しか動作しないように変更

--- jquery.fixedheaderscroll.js
+++ jquery.fixedheaderscroll.js
@@ -7,50 +7,54 @@
  * Released under the MIT license - http://opensource.org/licenses/MIT
  */
 
-(function( $ ) {
-	
-	$.fn.fixedHeaderScroll = function( options) {
-		
-		var settings = $.extend( {
-			'headerSelector': 'header', // You need to designate the header element in your website.
-			'offset':         0, // You can offset the scroll position.
-			'smooth':         true, // You can choose if you use smooth scroll or not.
-			'smoothDuration': 300, // Scroll speed. (in ms)
-			'smoothEasing':   'linear', // Scroll easing.
-		}, options);
-		
-		var fixScroll = function( targetSlector){
-			if( settings.headerSelector == false) {
-				var h = 0;
-			} else {
-				var headerEl = $( settings.headerSelector);
-				var h = headerEl.outerHeight();
-			}
-			
-			var targetEl = $( targetSlector);
-			var t = targetEl.offset().top;
-			var s = t - h + settings.offset;
-			
-			if( settings.smooth) {
-				$("html, body").animate({
-					scrollTop: s
-				}, settings.smoothDuration, settings.smoothEasing);
-			} else {
-				$("html, body").scrollTop( s);
-			}
-		}
-		
-		this.click( function(){
-			fixScroll( $(this).attr("href"));
-			return false;
-		});
+(function($) {
 
-		$( window).on('load',function(){
-			if( location.hash) {
-				fixScroll( location.hash);
-				return false;
-			}
-		});
+    $.fn.fixedHeaderScroll = function(options) {
 
-	};
-})( jQuery );
+        var settings = $.extend({
+            'headerSelector': 'header', // You need to designate the header element in your website.
+            'offset': 0, // You can offset the scroll position.
+            'smooth': true, // You can choose if you use smooth scroll or not.
+            'smoothDuration': 300, // Scroll speed. (in ms)
+            'smoothEasing': 'linear', // Scroll easing.
+        }, options);
+
+        function fixScroll(targetSlector) {
+            if (settings.headerSelector == false) {
+                var h = 0;
+            } else {
+                var headerEl = $(settings.headerSelector);
+                var h = headerEl.outerHeight();
+            }
+
+            var targetEl = $(targetSlector);
+            if( targetEl.size() < 1) {
+                return false;
+            }
+            var t = targetEl.offset().top;
+            var s = t - h + settings.offset;
+
+            if (settings.smooth) {
+                $("html, body").animate({
+                    scrollTop: s
+                }, settings.smoothDuration, settings.smoothEasing);
+            } else {
+                $("html, body").scrollTop(s);
+            }
+            return $(this);
+        }
+
+        this.unbind( "click.fixedHeaderScroll");
+        this.bind( "click.fixedHeaderScroll", function (){
+            fixScroll( $(this).attr("href"));
+        });
+
+        $(window).on('load', function() {
+            if (location.hash) {
+                fixScroll(location.hash);
+                return false;
+            }
+        });
+
+    };
+})(jQuery);
